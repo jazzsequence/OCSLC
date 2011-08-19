615b06791a7087caf9a936f01cf4f3c3390f3d9c
chris reynolds <chris@jazzsequence.com>
2011-08-19 12:04:20 -0600

    moved the xmission credit from the footer to the sidebar

--- footer.php
+++ footer.php
@@ -3,8 +3,7 @@
 <div id="footer" class="clearfloat">
 	<div id="copyright">
 		&#169; <?php echo date('Y'); ?> <span class="url fn org"><?php bloginfo('name'); ?></span> &bull; 134 North D Street, Salt Lake City, UT 801-578-8144 &bull; All Rights Reserved
-	</div>
-		<p><a href="http://xmission.com" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/xmission.gif" alt="Internet Services Donated by XMission" class="xmission-credit" /></p>
+	</div>		
 </div><!--END FOOTER-->
 </div><!--END PAGE-->
 <?php wp_footer(); ?>
