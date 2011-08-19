368fdfda871060ebce1331981705dea9f0aa6fd9
chris reynolds <chris@arcanepalette.com>
2011-08-19 10:16:12 -0600

    added logo to header added some initial css styling

--- header.php
+++ header.php
@@ -1,43 +1,29 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-
 <title><?php dynamictitles(); ?></title>
-
 <?php if (is_single() || is_page() ) : if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <meta name="description" content="<?php the_excerpt_rss(); ?>" />
 <?php endwhile; endif; elseif(is_home()) : ?>
 <meta name="description" content="<?php bloginfo('description'); ?>" />
 <?php endif; ?>
-
 <?php if(is_search()) { ?>
 <meta name="robots" content="noindex, nofollow" /> 
 <?php }?>
-
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen,projection" />
 <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/print.css" type="text/css" media="print" />
-
 <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
-
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?>" href="<?php bloginfo('rss2_url'); ?>" />
-
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/dropdowns.js"></script>
-
 <?php if ( is_singular() ) wp_enqueue_script('comment-reply'); // support for comment threading ?>
-
 <?php wp_head(); ?>
-
 </head>
-
 
 <body <?php if (is_home()) { ?>id="home" <?php } ?>class="<?php bodystyle(); ?>">
 
 <div id="page">
-
 <?php wp_nav_menu( array( 'menu_id' => 'nav', 'theme_location' => 'primary' ) ); ?>
 <?php /* commenting the old menus out
 <ul id="nav" class="clearfloat">
@@ -49,8 +35,8 @@
 <div id="wrapper" class="clearfloat">
 
 
-<div class="clearfloat" id="masthead">
-<h1><a href="<?php echo get_option('home'); ?>/" title="<?php _e('Return Home','Mimbo'); ?>"><?php bloginfo('name'); ?></a></h1>
+<div class="clearfloat" id="masthead"><a href="<?php echo home_url(); ?>"><img src="<?php bloginfo('stylesheet_url'); ?>/images/OC-logo.gif" alt="Salt Lake City Open Classroom" class="logo" /></a><br />
+<h1 class="sitetitle"><a href="<?php echo home_url(); ?>/" title="<?php _e('Return Home','Mimbo'); ?>"><?php bloginfo('name'); ?></a></h1>
 
 <div id="description"><?php bloginfo('description'); ?></div>
 
