e21b991b9b148c43bc36b767b3aade336b43001b
chris reynolds <chris@jazzsequence.com>
2011-08-23 16:03:45 -0600

    moved search box from header to sidebar there were some css changes in here, too, but they'll probably break now that I moved the searchbox

--- header.php
+++ header.php
@@ -28,10 +28,6 @@
 <?php wp_list_pages('title_li='); ?>
 </ul>
 */ ?>
-	<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-	<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" /><input type="image" src="<?php bloginfo('template_url'); ?>/images/magnify.gif" id="searchsubmit" />
-	</form>	
-
 <div id="wrapper" class="clearfloat">
 
 
--- sidebar.php
+++ sidebar.php
@@ -54,7 +54,9 @@
 
 <!--END SUBPAGE MENU-->
 
-
+	<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
+	<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" /><input type="image" src="<?php bloginfo('template_url'); ?>/images/magnify.gif" id="searchsubmit" />
+	</form>	
 
 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Mimbo Sidebar') ) : ?><?php endif; ?>
  		
